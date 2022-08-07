2443b8273f68006f5e7aaa4d18e85e2bf27e972a
themoneytoad <georgelteel@gmail.com>
2022-08-07 05:36:28 +0000

    adding the next map functionality, and removing old pixel editor

--- static/init.js
+++
@@ -1,9 +0,0 @@
-import { PyxledManager } from "./pyxledManager.js"
-
-window.addEventListener('load', function () {
-	const pyxlMan = new PyxledManager()
-	pyxlMan.init()
-	pyxlMan.newImage(16)
-	pyxlMan.startLoop()
-})
-
